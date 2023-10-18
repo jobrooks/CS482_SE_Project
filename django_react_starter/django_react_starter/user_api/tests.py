203cf06d1551c4f36d43a776b6f36ace54ccc22e
Joshua Brooks <Joshua Brooks>
2023-10-18 12:12:48 -0400

    Test function complete: expected fail

--- django_react_starter/django_react_starter/user_api/tests.py
+++ django_react_starter/django_react_starter/user_api/tests.py
@@ -10,3 +10,29 @@ class UsersManagersTest(TestCase):
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
+        try:
+            self.assertIsNone(user.username)
+        except AttributeError:
+            pass
+        with self.assertRaises(TypeError):
+            User.objects.create_user()
+        with self.assertRaises(TypeError):
+            User.objects.create_user(email="")
+        with self.assertRaises(ValueError):
+            User.objects.create_user(email="", password="foo")
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_user(email="super@user.com",password="foo")
+        self.assertEqual(admin_user.email, "super@user.com")
+        self.assertTrue(admin_user.is_active)
+        self.assertFalse(admin_user.is_staff)
+        self.assertFalse(admin_user.is_superuser)
+        try:
+            self.assertIsNone(admin_user.username)
+        except AttributeError:
+            pass
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(
+                email="super@user.com", password="foo", is_superuser=False
+            )
