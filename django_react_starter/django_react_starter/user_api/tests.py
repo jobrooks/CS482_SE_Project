df11f2d783f5f216038201a1196c1bc5e7fb8e81
Joshua Brooks <Joshua Brooks>
2023-10-22 16:15:31 -0400

    Added test for creating user

--- django_react_starter/django_react_starter/user_api/tests.py
+++ django_react_starter/django_react_starter/user_api/tests.py
@@ -1,4 +1,26 @@
-from django.contrib.auth import get_user_model
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from rest_framework import status
 
 # Create your tests here.
+class UserApitest(APITestCase):
+    def setUp(self):
+        self.test_user = User.objects.create_user('testuser', 'test@example.com', 'testpassword')
+
+        self.create_url = reverse('account-create')
+
+    def test_create_user(self):
+        data = {
+            'username': 'foobar',
+            'email':'foobar@example.com',
+            'password':'somepassword'
+        }
+
+        response = self.client.post(self.create_url, data, format='json')
+
+        self.assertEqual(User.objects.count(), 2)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['username'], data['username'])
+        self.assertEqual(response.data['email'], data['email'])
+        self.assertFalse('password' in response.data)
