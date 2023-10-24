7b29e751d456586411b9688fb00b30a6c598ba96
Kobe Middleton <kobe.middleton62@gmail.com>
2023-10-23 22:57:08 -0400

    I completely restructured our project. List of changes:  - added new 'backend' project  - removed 'fivecarddraw' app  - created basic html templates to allow for testing  - created views.py and urls.py to create basic pages

--- django_react_starter/django_react_starter/fivecarddraw/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import fivecarddraw
-
-class fivecarddrawAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'completed')
-
-# Register your models here.
-
-admin.site.register(fivecarddraw, fivecarddrawAdmin)
