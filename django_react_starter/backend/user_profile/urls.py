8dc6803df64bfa8454b16712243c5c6727978605
Joshua Brooks <Joshua Brooks>
2023-10-25 23:25:48 -0400

    Added view profile functionality

--- django_react_starter/backend/user_profile/urls.py
+++ django_react_starter/backend/user_profile/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     #path('register/', views.UserCreate.as_view(), name='account-create'),
-    path('profile/', views.get_user_profile),
+    path('profile/<str:username>/', views.get_user_profile, name ='get_user_profile'),
 ]
