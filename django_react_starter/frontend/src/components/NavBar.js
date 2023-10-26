e28f187cfaf523afe7f5b4c0f700a5676f49eea9
Collin Katz <collin.katz@gmail.com>
2023-10-25 22:35:07 -0400

    Have simple ui for opening navigation tools on page

--- django_react_starter/frontend/src/components/NavBar.js
+++ django_react_starter/frontend/src/components/NavBar.js
@@ -1,17 +1,66 @@
+import React from 'react';
 import {
     AppBar,
     Toolbar,
-    Button
+    Button,
+    IconButton,
+    Typography,
+    Drawer,
+    ClickAwayListener,
+    MenuItem
 } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 
-function NavBar() {
-    return (
-        <AppBar>
-            <Toolbar>
-                <Button>Help lol</Button>
-            </Toolbar>
-        </AppBar>
-    )
+class NavBar extends React.Component {
+
+    state = {
+        drawerOpen: false
+    };
+
+    // constructor(props) {
+    //     super(props);
+
+    // }
+
+    toggleDrawer = () => {
+        this.setState({ drawerOpen: !this.state.drawerOpen});
+        console.log("Toggling");
+    }
+
+    render() {
+        return (
+            <>
+                <AppBar>
+                    <Toolbar>
+                        <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                        onClick={this.toggleDrawer}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6">
+                            Five Card Draw
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                    <Drawer
+                        variant='temporary'
+                        open={this.state.drawerOpen}
+                        ModalProps={{
+                            keepMounted: true, // Better open performance on mobile.
+                            onBackdropClick: this.toggleDrawer
+                        }}
+                    >
+                        <Button>Help lol</Button>
+                        <MenuItem>drawer item 1</MenuItem>
+                    </Drawer>
+            </>
+        )
+    }
 }
 
 export default NavBar;
