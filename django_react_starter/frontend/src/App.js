99c5f4eab0c663da3f8ac9d22fe1a15471e15fbf
Collin Katz <collin.katz@gmail.com>
2023-10-26 02:41:09 -0400

    removed default stuff added routing

--- django_react_starter/frontend/src/App.js
+++ django_react_starter/frontend/src/App.js
@@ -2,25 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AccountSettings from './pages/AccountSettingsPage';
+import HomePage from './pages/HomePage';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 function App() {
+
+  const theme = createTheme({
+    spacing: 8,
+    padding: 4,
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <NavBar />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ThemeProvider theme={theme}>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path="/account" element={<AccountSettings />} />
+          </Routes>
+        </ThemeProvider>
     </div>
   );
 }
