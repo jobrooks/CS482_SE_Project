41b6757cebeacd4f8e6b7eb569d3831326b5e751
Kobe Middleton <kobe.middleton62@gmail.com>
2023-11-06 16:47:09 -0500

    - Added methods to create games, hands, and pots - Added methods to 'start' game, with hands distributed to users.

--- django_react_starter/backend/game/models.py
+++ django_react_starter/backend/game/models.py
@@ -30,8 +30,12 @@ RANK_CHOICES = (
 class Deck(models.Model):
     name = models.CharField(max_length=10)
 
+class User(models.Model):
+    money = models.IntegerField()
+
 class Hand(models.Model):
     name = models.CharField(max_length=10)
+    user = models.OneToOneField(User(), null=True, on_delete=models.CASCADE)
 
 class Card(models.Model):
     suit = models.CharField(max_length=1, choices=SUIT_CHOICES)
@@ -43,6 +47,7 @@ class Pot(models.Model):
     moneyAmount = models.IntegerField()
 
 class Game(models.Model):
+    name = models.CharField(max_length=10)
     deck = models.OneToOneField(Deck(), null=True, on_delete=models.CASCADE)
     pot = models.OneToOneField(Pot(), null=True, on_delete=models.CASCADE)
 
@@ -53,6 +58,22 @@ def create_deck():
         for y in RANK_CHOICES:
             card = Card(suit=x, rank=y, deck=deck)
             card.save()
+    return deck.pk
+
+def create_pot():
+    pot = Pot(moneyAmount=0)
+    pot.save()
+    return pot.pk
+
+def create_hand(name: str):
+    hand = Hand(name=name)
+    hand.save()
+    return hand.pk
+
+def create_user(money: int):
+    user = User(money=money)
+    user.save()
+    return user.pk
 
 def draw_card(deck: int, hand: int):
     deck_cards = list(Card.objects.filter(deck=deck))
@@ -69,3 +90,30 @@ def discard_card(cardsToBeDiscarded: [int], deck: int):
         deck_cards.deck = deck
         deck_cards.hand = None
         deck_cards.save()
+
+def place_bet(betAmount: int, potid: int, userid: int):
+    user = User.objects.get(pk=userid)
+    pot = Pot.objects.get(pk=potid)
+    pot.moneyAmount += betAmount
+    user.money -= betAmount
+
+def create_game(name: str, pot: Pot(), deck: Deck()):
+    game = Game(name=name, pot=pot, deck=deck)
+    game.save()
+    return game.pk
+
+def play_game(numPlayers: int, arrUsers: [int]):
+    deckid = create_deck()
+    potid = create_pot()
+    game = create_game(pot=Pot.objects.get(pk=potid), deck=Deck.objects.get(pk=deckid))
+
+    hand = []
+
+    for _ in range(0, numPlayers):
+        hand.append(create_hand())
+
+    
+    for id in hand:
+        for _ in range(0, 5):
+            draw_card(deckid, id)
+    
