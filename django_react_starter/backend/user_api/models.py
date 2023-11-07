c5b094fc97bee1c3b1cd122b345580a715e4c0bb
Joshua Brooks <Joshua Brooks>
2023-11-07 12:18:26 -0500

    fields added to updated user model

--- django_react_starter/backend/user_api/models.py
+++ django_react_starter/backend/user_api/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    
+    wins = models.models.PositiveIntegerField(default=0)
+    avatar = models.ImageField(null=True, blank=True)
+    bio = models.TextField(max_length=500, null=True, blank=True)
+    games_played = models.PositiveIntegerField(default=0)
     class Meta:
         db_table = 'auth_user'
