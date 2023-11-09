7bb5d0a09798ba809a44c77064c810e4f92f6026
Joshua Brooks <Joshua Brooks>
2023-11-09 02:22:00 -0500

    Profile page working, refacted profile view, created test for profile view

--- django_react_starter/backend/user_profile/views.py
+++ django_react_starter/backend/user_profile/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
 from user_api.models import User
+from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from django.core.serializers import serialize
+import json
 
 # Create your views here.
-def get_user_profile(request, username):
-    user = User.objects.get(username=username)
-    return render(request, 'profile.html', {"user":user})
+def get_user_profile(request, token):
+    try:
+        token_object = Token.objects.get(key=token)
+        user = token_object.user 
+
+        
+        user_data_json = serialize('json', [user], use_natural_primary_keys=True)
+        user_data = json.loads(user_data_json)[0]['fields']
+
+        
+        if user_data['avatar']:
+            user_data['avatar'] = user['avatar'].url  
+
+        return JsonResponse(user_data)
+    except Token.DoesNotExist:
+        return JsonResponse({"error": "User not found"}, status=404)
+    
