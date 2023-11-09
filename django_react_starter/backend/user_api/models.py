2ce90d76dd2ce7e1a5e31678ae154072b06a99a6
Joshua Brooks <Joshua Brooks>
2023-11-08 21:44:48 -0500

    Added to user model

--- django_react_starter/backend/user_api/models.py
+++ django_react_starter/backend/user_api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from game.models import Game
 # Create your models here.
 
 class User(AbstractUser):
@@ -7,5 +8,7 @@ class User(AbstractUser):
     avatar = models.ImageField(upload_to='avatars/', height_field=None, width_field=None, max_length=None, null=True, blank=True)
     bio = models.TextField(max_length=500, null=True, blank=True)
     games_played = models.PositiveIntegerField(default=0)
+    game_id = models.ForeignKey(Game,on_delete=models.CASCADE, null=True)
+    money = models.PositiveIntegerField(default=0)
     class Meta:
         db_table = 'auth_user'
