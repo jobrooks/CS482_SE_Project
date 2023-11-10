19521fb0a685d6d77f17e84e941be14819747f17
Collin Katz <collin.katz@gmail.com>
2023-11-09 23:23:36 -0500

    got a url for getting user table theme and for putting it also

--- django_react_starter/backend/user_profile/urls.py
+++ django_react_starter/backend/user_profile/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     #path('register/', views.UserCreate.as_view(), name='account-create'),
     path('profile/<str:username>/', views.get_user_profile, name ='get_user_profile'),
+    path('profile/tabletheme/<str:token>/', views.TableTheme.as_view(), name='table_theme'),
+    path('profile/getusers', views.UserList.as_view(), name='get_users'),
+    # path('profile/gettabletheme/<str:token>/', views.get_table_theme, name='get_table_theme')
 ]
