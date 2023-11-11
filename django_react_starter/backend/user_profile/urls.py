c639056e947a5ed368e116d68a8d546a3d17eed1
Collin Katz <collin.katz@gmail.com>
2023-11-11 16:58:37 -0500

    removed unnecessary urls

--- django_react_starter/backend/user_profile/urls.py
+++ django_react_starter/backend/user_profile/urls.py
@@ -2,11 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Other URL patterns...
     path('profile/<str:token>/', views.get_user_profile, name='get_user_profile'),
-    #path('register/', views.UserCreate.as_view(), name='account-create'),
-    path('profile/<str:username>/', views.get_user_profile, name ='get_user_profile'),
     path('profile/tabletheme/<str:token>/', views.TableTheme.as_view(), name='table_theme'),
-    path('profile/getusers', views.UserList.as_view(), name='get_users'),
-    # path('profile/gettabletheme/<str:token>/', views.get_table_theme, name='get_table_theme')
 ]
