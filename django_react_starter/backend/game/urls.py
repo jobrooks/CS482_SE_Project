be76a4c5f01b50243f3733f28d76e7a59105a2aa
Kobe Middleton <kobe.middleton62@gmail.com>
2023-11-10 21:07:10 -0500

    added urls to match views

--- django_react_starter/backend/game/urls.py
+++ django_react_starter/backend/game/urls.py
@@ -3,12 +3,12 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    # path("cardview/", views.cardView),
-    # path("handview/", views.handView),
-    # path("deckview/", views.deckView),
     path("player/<int:pk>", views.PlayerDetail.as_view()),
     path("player/", views.PlayerList.as_view()),
     path("game/<int:pk>/", views.GameDetail.as_view()),
     path("game/", views.GameList.as_view()),
     path("hand/<int:pk>/", views.HandDetail.as_view()),
+    path("hand/", views.HandList.as_view()),
+    path("deck/", views.DeckList.as_view()),
+    path("deck/<int:pk>", views.DeckDetail.as_view()),
 ]
