487b25102af4920f9c86bcedd99148882a7b7f23
Joshua Brooks <Joshua Brooks>
2023-11-11 16:01:36 -0500

    Added test

--- django_react_starter/backend/friend/tests.py
+++ django_react_starter/backend/friend/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from user_api.models import User
+from rest_framework.authtoken.models import Token
 
-# Create your tests here.
+class FriendTest(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.token.key}')
+
+    def test_search_users(self):
+        url = 'http://localhost:8000/friend/search_users/'
+        response = self.client.get(url, {'username': 'searched_user'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
