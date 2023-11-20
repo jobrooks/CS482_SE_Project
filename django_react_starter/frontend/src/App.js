489c218c8d037cc7ede29f4fb2ae6bd70cd50d28
Sajiv <jsgnanasekaran@loyola.edu>
2023-11-20 17:15:54 -0500

    removed logo.svg

--- django_react_starter/frontend/src/App.js
+++ django_react_starter/frontend/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
