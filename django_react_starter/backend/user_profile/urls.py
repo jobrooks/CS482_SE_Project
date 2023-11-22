e247c12ffa2e3323c1a39356612a9ec28db4379f
Collin Katz <collin.katz@gmail.com>
2023-11-22 13:37:26 -0500

    Fixed issues with urls

--- django_react_starter/backend/user_profile/urls.py
+++ django_react_starter/backend/user_profile/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('profile/<str:token>/', views.GetUserProfile.as_view(), name='get_user_profile'),
     path('profile/tabletheme/<str:token>/', views.TableTheme.as_view(), name='table_theme'),
     path('profile/cardbacking/<str:token>/', views.CardBacking.as_view(), name='card_backing'),
-    path('profile/leaderboard/<str:token>/', views.Leaderboard.as_view(), name='leaderboard')
+    path('profile/leaderboard', views.Leaderboard.as_view(), name='leaderboard')
 ]
