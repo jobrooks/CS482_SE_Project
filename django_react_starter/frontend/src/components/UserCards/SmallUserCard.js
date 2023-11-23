7fe4ac8dee4cfea7df92d6c2f727c4d3bb92cb6d
Collin Katz <collin.katz@gmail.com>
2023-11-22 21:58:43 -0500

    Now doing conditional rendering info type vs friendable type also handling button clicks now also fixed formatting

--- django_react_starter/frontend/src/components/UserCards/SmallUserCard.js
+++ django_react_starter/frontend/src/components/UserCards/SmallUserCard.js
@@ -1,5 +1,7 @@
-import { Avatar, Card, CardContent, Stack, Item, Typography, Divider, CardActionArea, Button, Badge } from "@mui/material";
+import { Avatar, Card, CardContent, Stack, Item, Typography, Divider, CardActionArea, Button, Badge, IconButton, Box } from "@mui/material";
 import React from "react";
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
 
 /** SmallUserCard
  * - This is a user card object that displays user information in a small size
@@ -14,94 +16,149 @@ class SmallUserCard extends React.Component {
 
     constructor(props) {
         super(props);
+        this.handleInfoClick = this.handleInfoClick.bind(this);
+        this.handleAddFriend = this.handleAddFriend.bind(this);
         this.state = {
             avatarColor: this.props.avatarColor,
             username: this.props.username,
             wins: this.props.wins,
             
             info: this.props.info,
+            friendable: this.props.friendable,
+        }
+    }
+
+    handleInfoClick() {
+        console.log("Clicked");
+    }
+
+    handleAddFriend() {
+        console.log("Add Friend");
+    }
+
+    getAddFriendIcon() {
+        if (this.state.friendable) {
+            return (
+                <IconButton
+                    aria-label="Add Friend"
+                    onClick={this.handleAddFriend}
+                >
+                    <PersonAddIcon />
+                </IconButton>
+            );
         }
     }
 
     render() {
+        let mainComponent = (
+            <Card elevation={3}
+                sx={{
+                    width: 400,
+                    height: "auto",
+                    m: 0
+                }}
+            >
+                <CardContent
+                    sx={{
+                        pb: "16px"
+                    }}
+                >
+                    <Stack
+                        direction="row"
+                        divider={<Divider orientation="vertical" flexItem />}
+                        spacing={0}
+                        alignItems="center"
+                        justifyContent="space-between"
+                    >
+                        <Badge
+                            color="success"
+                            badgeContent=" "
+                            variant="dot"
+                            size="large"
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'right',
+                            }}
+                        >
+                            <Avatar
+                                sx = {{
+                                    bgcolor: this.state.avatarColor,
+                                    width: 50,
+                                    height: 50,
+                                }}
+                            />
+                        </Badge>
+                        <Stack direction="column"
+                            alignItems="flex-start"
+                        >
+                            <Typography variant="caption" noWrap={false}>
+                                Username
+                            </Typography>
+                            <Typography variant="subtitle" noWrap={false}>
+                                {this.state.username}
+                            </Typography>
+                        </Stack>
+                        <Stack direction="column"
+                            alignItems="center"
+                        >
+                            <Typography variant="caption" noWrap={false}>
+                                <EmojiEventsIcon />
+                            </Typography>
+                            <Typography variant="h6" noWrap={false}>
+                                {this.state.wins}
+                            </Typography>
+                        </Stack>
+                        { this.getAddFriendIcon() }
+                    </Stack>
+                </CardContent>
+                <CardActionArea>
+                    {/*
+                        Empty CardActionArea is necessary to center cardcontent
+                        Card gives last item in card an extra padding idk why
+                        ^ just default mui formatting
+                    */}
+                </CardActionArea>
+            </Card>
+        );
+
         return (
             <div className="UserCard">
-                <Button
-                    disabled={!this.state.info}
-                    sx={{
-                        p: 0,
-                        m: 2,
-                        textTransform: "none",
-                    }}
-                >
-                <Card elevation={3}
-                    sx={{
-                        width: 350,
-                        height: "auto",
-                        m: 0
-                    }}
-                >
-                    <CardContent
+                {/*
+                    This ternary operator conditionally renders the main button
+                    that the entire card component is in if info is true to enable
+                    the user to open the info dialog with the button. Otherwise
+                    it only renders the component in a box.
+                */}
+                { this.state.info ?
+                    <Box
                         sx={{
-                            pb: "16px"
+                            p: 0,
+                            m: 2,
+                            textTransform: "none",
                         }}
                     >
-                        <Stack
-                            direction="row"
-                            divider={<Divider orientation="vertical" flexItem />}
-                            spacing={0}
-                            alignItems="center"
-                            justifyContent="space-around"
+                        <Button
+                            onClick={this.handleInfoClick}
+                            sx={{
+                                p: 0,
+                                m: 0,
+                                textTransform: "none",
+                            }}
                         >
-                            <Badge
-                                color="success"
-                                badgeContent=" "
-                                variant="dot"
-                                size="large"
-                                anchorOrigin={{
-                                    vertical: 'bottom',
-                                    horizontal: 'right',
-                                }}
-                            >
-                                <Avatar
-                                    sx = {{
-                                        bgcolor: this.state.avatarColor,
-                                        width: 50,
-                                        height: 50,
-                                    }}
-                                />
-                            </Badge>
-                            <Stack direction="column"
-                                alignItems="flex-start"
-                            >
-                                <Typography variant="caption" noWrap={false}>
-                                    Username
-                                </Typography>
-                                <Typography variant="h6" noWrap={false}>
-                                    {this.state.username}
-                                </Typography>
-                            </Stack>
-                            <Stack direction="column"
-                                alignItems="flex-start"
-                            >
-                                <Typography variant="caption" noWrap={false}>
-                                    Wins
-                                </Typography>
-                                <Typography variant="h6" noWrap={false}>
-                                    {this.state.wins}
-                                </Typography>
-                            </Stack>
-                        </Stack>
-                    </CardContent>
-                    <CardActionArea>
-                        {/*
-                            Empty CardActionArea is necessary to center cardcontent
-                            Card gives last item in card an extra padding idk why
-                            ^ just default mui formatting
-                        */}
-                    </CardActionArea>
-                </Card>
-                </Button>
+                            { mainComponent }
+                        </Button>
+                    </Box>
+                :
+                    <Box
+                        sx={{
+                            p: 0,
+                            m: 2,
+                            textTransform: "none",
+                        }}
+                    >
+                        { mainComponent }
+                    </Box>
+                }
             </div>
         )
     }
