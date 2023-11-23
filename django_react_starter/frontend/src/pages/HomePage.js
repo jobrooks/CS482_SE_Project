96189da83bd71a9f77f98f1cd119f7f0faa52785
Collin Katz <collin.katz@gmail.com>
2023-11-23 14:53:31 -0500

    added editable field

--- django_react_starter/frontend/src/pages/HomePage.js
+++ django_react_starter/frontend/src/pages/HomePage.js
@@ -23,6 +23,8 @@ class HomePage extends React.Component {
                         <UserCard />
                         <LargeUserCard
                             username="collinkatz"
+                            friendable={true}
+                            editable={false}
                         />
                     </Grid>
                     <Grid item xs={6}>
