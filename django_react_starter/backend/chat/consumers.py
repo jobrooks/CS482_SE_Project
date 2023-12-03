f3c50273bdb647dff537df418ccda9d942dde995
Collin Katz <collin.katz@gmail.com>
2023-12-03 18:19:20 -0500

    Added backend models for chat and messages and rooms and chatters as well as some basic functions for sending messages and getting chat history for a room

--- django_react_starter/backend/chat/consumers.py
+++ django_react_starter/backend/chat/consumers.py
@@ -1,17 +1,25 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
+from chat.models import ChatRoom
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
-        # Join the group
-        self.group_name = "chatter"
-        
-        async_to_sync(self.channel_layer.group_add)(
-            self.group_name, self.channel_name
-        )
-        
-        self.accept()
+        # try:
+            # Join the group
+            self.group_name = "global"
+            self.chat_type = "global"
+            
+            async_to_sync(self.channel_layer.group_add)(
+                self.group_name, self.channel_name
+            )
+            
+            self.chat_room = ChatRoom.get(self.group_name, self.chat_type)
+            
+            self.accept()
+        # except:
+            # print("Could not join group: " + self.group_name)
+            # self.send(text_data=json.dumps({"error": "Could not join group: " + self.group_name}))
 
     def disconnect(self, close_code):
         # Leave the group
@@ -20,12 +28,17 @@ class ChatConsumer(WebsocketConsumer):
         )
 
     def receive(self, text_data):
-        serialized_text_data = json.loads(text_data)
-        # Send the data to the group.
-        async_to_sync(self.channel_layer.group_send)(
-            self.group_name, {"type": "message_notice", "serialized_text_data": serialized_text_data}
-        )
-        # self.send(text_data=text_data)
+        try:
+            serialized_text_data = json.loads(text_data)
+            # Send the data to the group.
+            async_to_sync(self.channel_layer.group_send)(
+                self.group_name, {"type": "message_notice", "serialized_text_data": serialized_text_data}
+            )
+            
+            self.chat_room.send_message(serialized_text_data)
+            
+        except:
+            self.send(text_data=json.dumps({"sender": "error", "avatar_color": "#000000", "message": "**Message did not send**"}))
     
     # this is necessary to output the recieved message to your own websocket
     # it does result in echoing on your end when you send a message though, but
---
+++ django_react_starter/backend/chat/models.py
@@ -0,0 +1,86 @@
+from django.db import models
+from user_api.models import User
+
+class ChatRoom(models.Model):
+    CHAT_TYPE_CHOICES = [
+        ("global", "Global"),
+        ("game", "Game"),
+        ("friend", "Friend"),
+    ]
+    
+    group_name = models.CharField(max_length=255, primary_key=True)
+    chat_type = models.CharField(max_length=255, choices=CHAT_TYPE_CHOICES, null=True, blank=True)
+    
+    def get_ChatRoom(group_name, chat_type):
+        try:
+            return ChatRoom.objects.get(group_name=group_name, chat_type=chat_type)
+        except ChatRoom.DoesNotExist:
+            return None
+    
+    def get_chat_history(self):
+        messages = Message.objects.filter(chat_room=ChatRoom.objects.get(group_name=self.group_name))
+        return messages
+    
+    def send_message(self, serialized_text_data):
+        new_message = Message.create(serialized_text_data, ChatRoom.objects.get(group_name=self.group_name), Chatter.get(serialized_text_data["sender"], ChatRoom.objects.get(group_name=self.group_name)))
+        
+    def get(group_name, chat_type):
+        existing_chat = ChatRoom.get_ChatRoom(group_name=group_name, chat_type=chat_type)
+        if existing_chat:
+            return existing_chat
+        else: # Make a new chat room
+            chat_room = ChatRoom(group_name=group_name, chat_type=chat_type)
+            chat_room.save()
+            return chat_room
+            
+    # def __init__(self, group_name, chat_type):
+    #     self.group_name = group_name
+    #     self.chat_type = chat_type
+    #     self.save()
+    
+    def __str__(self):
+        return "group_name: " + self.group_name + " type: " + self.chat_type
+        
+
+class Chatter(models.Model):
+    
+    user = models.OneToOneField(User(), on_delete=models.CASCADE)
+    chat_room = models.ForeignKey(ChatRoom(), on_delete=models.CASCADE)
+    
+    def get_Chatter(user, chat_room):
+        try:
+            return Chatter.objects.get(user=user, chat_room=chat_room)
+        except Chatter.DoesNotExist:
+            return None
+    
+    def get(username, chat_room):
+        user = User.objects.get(username=username)
+        if user: # Check user exists
+            chatting_user = Chatter.get_Chatter(user=user, chat_room=chat_room)
+            if chatting_user:
+                return chatting_user
+            else:
+                chatter = Chatter(user=user, chat_room=chat_room)
+                chatter.save()
+                return chatter
+        else:
+            print("User DNE") # May cause errors if nothing is returned think about returning chatter from generic guest user
+    
+    def __str__(self):
+        return self.user.username
+
+class Message(models.Model):
+    sender = models.TextField(max_length=500, null=True, blank=True)
+    avatar_color = models.TextField(max_length=500, null=True, blank=True)
+    message = models.TextField(max_length=500, null=True, blank=True)
+    chat_room = models.ForeignKey(ChatRoom(), on_delete=models.CASCADE)
+    chatter = models.ForeignKey(Chatter(), on_delete=models.CASCADE)
+    
+    def create(serialized_text_data, chat_room, chatter):
+        message = Message(sender=serialized_text_data["sender"], avatar_color=serialized_text_data["avatar_color"], message=serialized_text_data["message"],
+                       chat_room=chat_room, chatter=chatter)
+        message.save()
+        return message
+    
+    def __str__(self):
+        return self.sender + ": " + self.message
