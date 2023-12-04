0c22340422ebb2ba3932d4e1983557c8af7479bd
Collin Katz <collin.katz@gmail.com>
2023-12-04 04:15:52 -0500

    Now can see multiple types of chat. Big overhaul, fully functional thank god

--- django_react_starter/frontend/src/components/Chat.js
+++ django_react_starter/frontend/src/components/Chat.js
@@ -1,17 +1,17 @@
 import React from "react";
 import axios from "axios";
 import { ChatBox, ReceiverMessage, SenderMessage } from "mui-chat-box";
-import { Avatar, Grid, Icon, IconButton, Paper, Stack, TextField, Typography } from "@mui/material";
+import { AppBar, Avatar, Button, ButtonGroup, CardHeader, Grid, Icon, IconButton, Paper, Stack, Tab, Tabs, TextField, Typography } from "@mui/material";
+import TabContext from '@mui/lab/TabContext';
+import TabPanel from '@mui/lab/TabPanel';
 import SendIcon from '@mui/icons-material/Send';
 import SmallUserCard from "./UserCards/SmallUserCard";
 
 /**
  * Chat box will allow users to chat with eachother.
- * "pal" is the prefix for the other person a user is chatting with.
+ * Depricated: "pal" is the prefix for the other person a user is chatting with.
  * I derrived the name from "penpal."
  */
-const roomName = "global";
-
 class Chat extends React.Component {
 
     constructor(props) {
@@ -23,21 +23,23 @@ class Chat extends React.Component {
         this.getFriendChatList = this.getFriendChatList.bind(this);
         this.getFriends = this.getFriends.bind(this);
         this.getWebsocketConnection = this.getWebsocketConnection.bind(this);
+        this.handleTabChange = this.handleTabChange.bind(this);
         this.bottomRef = null;
-        // this.onIncomingMessage = this.onIncomingMessage.bind(this);
         this.state = {
             // Websocket
-            chatSocket: new WebSocket(`ws://localhost:8000/ws/chat/${roomName}/`),
+            chatSocket: null,
 
             // User Data
             myUsername: null,
             myUserdata: null,
-            palUsername: this.props.palUsername,
-            palUserData: null,
+            // palUsername: this.props.palUsername,
+            // palUserData: null,
 
             // Internal State
-            mode: this.props.mode, // [ friend, global, game ]
-            gameId: this.props.gameId, // pass this as well if mode is game
+            chatType: this.props.chatType, // [ global, friend, game ]
+            groupName: this.props.groupName, // pass this as well if chatType is game or global
+            gameId: this.props.gameId,
+            currentTab: 0, // tab: [ 0, 1, 2 ] -> chatType: [ "global", "game", "friend" ]
             chattable: false, // Could use this to determine if user should be able to chat
             isLoading: true,
             textFieldData: "",
@@ -48,7 +50,7 @@ class Chat extends React.Component {
 
     componentDidMount() {
         this.setState((prevState) => ({
-            chattable: prevState.mode !== "friend"
+            chattable: prevState.chatType !== "friend"
         }));
         this.bottomRef = React.createRef();
         // Synchronously runs getting data stuff
@@ -60,6 +62,10 @@ class Chat extends React.Component {
 
     componentDidUpdate() {
         this.scrollToBottom();
+    }
+
+    componentWillUnmount() {
+        this.closeWebsocketConnection();
     }
     
     onIncomingMessage(event) {
@@ -73,14 +79,45 @@ class Chat extends React.Component {
 
     getWebsocketConnection() {
         return new Promise((resolve, reject) => {
-            this.setState({ chatSocket: new WebSocket(`ws://localhost:8000/ws/chat/${roomName}/`) }, () => {
-                this.state.chatSocket.addEventListener("message", this.onIncomingMessage.bind(this));
-                resolve("Finished getting websocket connection");
+            let ws = null;
+            new Promise((resolve, reject) => {
+                ws = new WebSocket(`ws://localhost:8000/ws/chat/${this.state.groupName}/${this.state.chatType}`);
+                ws.onopen = (event) => { // Initialize chat to global when opened
+                    resolve("Finished connecting to websocket");
+                }
+            })
+            .then(() => {
+                this.setState({ chatSocket: ws }, () => {
+                    this.state.chatSocket.addEventListener("message", this.onIncomingMessage.bind(this));
+                    resolve("Finished getting websocket connection");
+                });
             });
         });
     }
 
-    getUserDatas() {
+    setChatGroup(groupName, chatType) { // Sends a message to server to change group to that specified
+        return new Promise((resolve, reject) => {
+            this.setState({ groupName: groupName, chatType: chatType, chatData: [] }, () => {
+                this.bottomRef = React.createRef();
+                this.closeWebsocketConnection()
+                .then(this.getWebsocketConnection)
+                .then(this.getChatHistory)
+                .finally(resolve("Set new chat group"));
+            });
+        });
+        // Depricated way to change chat: this.state.chatSocket.send(JSON.stringify( {sender: this.state.myUsername, type: "set_group", group_name: groupName, chat_type: chatType} ));
+    }
+
+    closeWebsocketConnection() {
+        return new Promise((resolve, reject) => {
+            this.state.chatSocket.close(1000, "User closed websocket");
+            this.setState({ chatSocket: null }, () => {
+                resolve("Finished closing websocket");
+            });
+        });
+    }
+
+    getUserDatas() { // Pal depricated
         return new Promise((resolve, reject) => {
             Promise.all([ // Array of two promises that resolves the outer promise when they're finished
                 // Get my user data
@@ -93,13 +130,13 @@ class Chat extends React.Component {
                     console.log("Error getting my user data");
                 }),
                 // Get pals user data
-                axios.get(`http://localhost:8000/user_profile/profile/getuserprofile/${this.state.palUsername}`)
-                .then((response) => {
-                    this.setState({ palUserData: response.data });
-                })
-                .catch((response) => {
-                    console.log("Error getting pal user data");
-                }),
+                // axios.get(`http://localhost:8000/user_profile/profile/getuserprofile/${this.state.palUsername}`)
+                // .then((response) => {
+                //     this.setState({ palUserData: response.data });
+                // })
+                // .catch((response) => {
+                //     console.log("Error getting pal user data");
+                // }),
             ]).finally(() => {
                 resolve("Finished getting user data");
             })
@@ -126,7 +163,7 @@ class Chat extends React.Component {
 
     getChatHistory() {
         return new Promise((resolve, reject) => {
-            axios.get(`http://localhost:8000/chat/get_chat_history/${roomName}`)
+            axios.get(`http://localhost:8000/chat/get_chat_history/${this.state.groupName}`)
             .then((response) => {
                 this.setState({ chatData: response.data.messages });
             })
@@ -156,52 +193,53 @@ class Chat extends React.Component {
             let avatar_color = this.state.chatData[i].avatar_color;
             if (sender === this.state.myUsername) { // Sender is me
                 listBuffer.push(
-                    <SenderMessage avatar={<SmallUserCard username={sender} avatarColor={avatar_color} info={true} isButton={true} />}>
-                        { message }
-                    </SenderMessage>
+                    // You have to put it in a chat box so it fills the whole space available
+                    <Grid container direction="column" sx={{width: "95%"}}>
+                        <SenderMessage avatar={<SmallUserCard username={sender} avatarColor={avatar_color} info={true} isButton={true} />}>
+                            { message }
+                        </SenderMessage>
+                    </Grid>
                 );
             } else {
                 listBuffer.push(
-                    <ReceiverMessage avatar={<SmallUserCard username={sender} avatarColor={avatar_color} info={true} isButton={true} />}>
-                        { message }
-                    </ReceiverMessage>
+                    <Grid container direction="column" sx={{width: "95%"}}>
+                        <ReceiverMessage avatar={<SmallUserCard username={sender} avatarColor={avatar_color} info={true} isButton={true} />}>
+                            { message }
+                        </ReceiverMessage>
+                    </Grid>
                 );
             }
             // listBuffer.push(<div className="stack-bottom" ref={ this.bottomRef } />)
         }
         return (
-            <>
+            <Stack direction="column" ref={ this.bottomRef } spacing={0} sx={{ maxHeight: 400, overflow: "auto", width: "auto"}}>
                 { listBuffer }
-            </>
+            </Stack>
         );
     }
 
     getFriends() {
         return new Promise((resolve, reject) => {
-            if (this.state.mode === "friend") {
-                let token = JSON.parse(localStorage.getItem("sessionToken"));
+            let token = JSON.parse(localStorage.getItem("sessionToken"));
 
-                let config = {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
-                };
+            let config = {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            };
 
-                axios.get("http://localhost:8000/friend/get_friends/", config)
-                .then((response) => {
-                    console.log(response.data.friends);
-                    this.setState({ friends: response.data.friends });
-                })
-                .catch((response) => {
-                    console.log("Error getting friends");
-                    console.error(response);
-                })
-                .finally(() => {
-                    resolve("Finished getting friends");
-                });
-            } else {
-                resolve("Finished getting friends: not in friend mode");
-            }
+            axios.get("http://localhost:8000/friend/get_friends/", config)
+            .then((response) => {
+                console.log(response.data.friends);
+                this.setState({ friends: response.data.friends });
+            })
+            .catch((response) => {
+                console.log("Error getting friends");
+                console.error(response);
+            })
+            .finally(() => {
+                resolve("Finished getting friends");
+            });
         });
     }
 
@@ -216,6 +254,7 @@ class Chat extends React.Component {
                     is_active={friend.is_active}
                     avatarColor={friend.avatar_color}
                     info={false}
+                    isThin={true}
                     friendable={false}
                     inviteable={false}
                     messageable={true}
@@ -227,6 +266,7 @@ class Chat extends React.Component {
             <div id="friendTable">
                 <Stack direction="column"
                     alignItems="center"
+                    ref={ this.bottomRef }
                     sx={{
                         width: "auto",
                         maxHeight: "100%",
@@ -240,60 +280,104 @@ class Chat extends React.Component {
     }
 
     handleOpenFriendChat(username) {
-        
+        this.setState({ currentTab: 2, chatType: "friend" });
+        let usernameList = [username, this.state.myUsername];
+        usernameList = usernameList.sort().join("_"); // Sort the two alphabetically and join them with an _ (Note: websocket url's cannot contain -) this way either friend can get the right group_name
+        this.setChatGroup(usernameList, "friend")
+        .then(() => {
+            this.setState({ friendChatOpen: true })
+        })
+    }
+
+    handleTabChange(event, newValue) {
+        let valueToChatType = {0: "global", 1: "game", 2: "friend"}
+        let newChatType = valueToChatType[newValue]
+        console.log(newChatType)
+        this.setState({ currentTab: newValue, friendChatOpen: false }, () => {
+            if (newChatType === "global") {
+                this.setChatGroup("global", newChatType)
+            } else if (newChatType === "game") {
+                this.setChatGroup(this.state.gameId, newChatType)
+            } else if (newChatType === "friend") {
+                // Do nothing until friend is selected
+            }
+        });
     }
 
     render() {
-        let chatBoxInterface = (
-            <Paper elevation={3}
-                    sx={{
-                        p: 1,
-                        m: 2,
-                        width: 430,
-                        height: "auto",
-                    }}
-                >
-                    <Stack direction="column" ref={ this.bottomRef } spacing={2} sx={{maxHeight: 400, overflow: "auto", width: "auto"}}>
-                        { this.makeMessagesFromChatData() }
-                    </Stack>
-                    { this.state.chattable ?
-                        <Stack direction="row" justifyContent="space-around">
-                            <TextField
-                                id="chat-entry"
-                                label="Send a Message"
-                                variant="outlined"
-                                onChange={ this.handleMessageChange }
-                                onKeyDown={ this.handleEnterPressed }
-                                value={ this.state.textFieldData }
-                                sx={{
-                                    m: 2,
-                                    width: "80%",
-                                    height: "auto",
-                                }}
-                            />
-                            <IconButton
-                                onClick={ this.handleSend }
-                            >
-                                <SendIcon />
-                            </IconButton>
-                        </Stack>
-                        :
-                        <></>
-                    }
-                </Paper>
-        );
         let friendInterface = (
             <div id="friendInterface">
                 { this.getFriendChatList() }
             </div>
         );
+        let chatBoxInterface = (
+            <Paper elevation={3}
+                sx={{
+                    p: 1,
+                    m: 2,
+                    width: 430,
+                    height: "auto",
+                }}
+            >
+                <TabContext value={this.state.currentTab}>
+                    <Tabs
+                        value={this.state.currentTab}
+                        onChange={this.handleTabChange}
+                        indicatorColor="primary"
+                        textColor="primary"
+                        variant="fullWidth"
+                        aria-label="chat tabs"
+                    >
+                        <Tab label="Global" />
+                        <Tab label="Game" disabled={!this.state.gameId}/>
+                        <Tab label="Friends" />
+                    </Tabs>
+                    <TabPanel value={0}>
+                        { this.makeMessagesFromChatData() }
+                    </TabPanel>
+                    <TabPanel value={1}>
+                        { this.makeMessagesFromChatData() }
+                    </TabPanel>
+                    <TabPanel value={2}>
+                        { this.state.friendChatOpen ?
+                            <>
+                                { this.makeMessagesFromChatData() }
+                            </>
+                        :
+                            friendInterface
+                        }
+                    </TabPanel>
+                    
+                </TabContext>
+                { this.state.chattable ?
+                    <Stack direction="row" justifyContent="space-around">
+                        <TextField
+                            id="chat-entry"
+                            label="Send a Message"
+                            variant="outlined"
+                            onChange={ this.handleMessageChange }
+                            onKeyDown={ this.handleEnterPressed }
+                            value={ this.state.textFieldData }
+                            sx={{
+                                m: 2,
+                                width: "80%",
+                                height: "auto",
+                            }}
+                        />
+                        <IconButton
+                            onClick={ this.handleSend }
+                        >
+                            <SendIcon />
+                        </IconButton>
+                    </Stack>
+                    :
+                    <></>
+                }
+            </Paper>
+        );
         return (
             <div id="chat">
-                { this.state.mode === "friend" ?
-                    friendInterface
-                :
-                    chatBoxInterface
-                }
+                {chatBoxInterface}
             </div>
         );
     }
--- django_react_starter/frontend/src/pages/HomePage.js
+++ django_react_starter/frontend/src/pages/HomePage.js
@@ -32,7 +32,7 @@ class HomePage extends React.Component {
                             messageable={false}
                             inviteable={false}
                         />
-                        <Chat palUsername="smelly" mode="global" />
+                        <Chat groupName="global" chatType="global" />
                         <TableCard 
                             token={JSON.parse(localStorage.getItem("sessionToken"))}
                             guestUsername={JSON.parse(localStorage.getItem("guestUsername"))}
